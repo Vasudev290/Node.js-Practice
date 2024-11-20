ba1eb04c5c877ab1d3059aea15b3862831ccb126
Vasudev290 <vasukesavulu@gmail.com>
2024-11-20 11:22:34 +0530

    events

--- NodeJS Version 2.0/Core_Modules/Events/events.js
+++ NodeJS Version 2.0/Core_Modules/Events/events.js
@@ -2,12 +2,28 @@ const Events = require("events")
 
 const EventsEm = new Events();
 
+//Listening for events;
 EventsEm.on("events", () => {
     console.log("listening for events..!");
 });
+//Trigger the events
 EventsEm.emit("events")
+
 
 EventsEm.on("payment", (plan) => {
     console.log("trigger payments", plan);
 })
 EventsEm.emit("payment", "Gold")
+EventsEm.emit("payment", "Silver")
+
+
+/* EventsEm.on("removeListenter", () => {
+    console.log("Removeing event listnet");
+}); */
+const myListner = () => {
+    console.log("Listening for events..!");
+}
+EventsEm.on("removeListner", myListner)
+EventsEm.emit("RemoveListner");
+EventsEm.removeListener("removeListner", myListner);
+EventsEm.emit("removeListner")
