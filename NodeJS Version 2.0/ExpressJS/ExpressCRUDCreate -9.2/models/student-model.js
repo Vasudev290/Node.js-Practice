4545f9fb346db5e4054f459c86bdcb773336454d
Vasudev290 <vasukesavulu@gmail.com>
2024-11-23 15:32:52 +0530

    MongoDB Create Method

---
+++ NodeJS Version 2.0/ExpressJS/ExpressCRUDCreate -9.2/models/student-model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var studentsSchema = mongoose.Schema({
+    studentId: Number,
+    firstName:String,
+    lastName:String,
+    age:Number,
+    department:String,
+    BloodGroup: String,
+    motherName:String,
+    fatherName:String,
+    schoolName:String
+  });
+  
+  var StudentModel = mongoose.model("Student", studentsSchema);
+
+  module.exports = StudentModel;
