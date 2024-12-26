198fc727c4ce307b340b4e3d0d67b02dc4e24d83
Vasudev290 <vasukesavulu@gmail.com>
2024-12-26 12:14:04 +0530

    CRUD

--- NodeJS Version 2.0/ExpressCRUDSir/router/apiRouter.js
+++ NodeJS Version 2.0/ExpressCRUDSir/router/apiRouter.js
@@ -9,19 +9,19 @@ Method:POST
 Required Fields:None 
 Access Type:Public 
 */
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     let emp_data = req.body;
-    console.log(emp_data)
-    let employees = getEmployee();
-    let emp = employees.find((employee) => {
-        return employee.empId == emp_data.eid})
-        console.log(emp_data)
+    //console.log(emp_data)
+    let employees = await getEmployee();
+    let emp = employees.find((employee) => 
+         employee.eid == emp_data.eid);
+        //console.log(emp_data)
     if(emp) {
         return res.json({"Message": "Employee Already existed"})
     }
     employees.push(emp_data)
-    console.log(employees)
-    createEmployee(employees)
+    //console.log(employees)
+    await createEmployee(employees)
 
     return res.json({"msg": "New Employee Created"})
 })
@@ -38,12 +38,46 @@ router.get('/read', async (req, res) => {
 })
 
 /*
+Usage: get  emp/product/order based on id
+API URL: http://127.0.0.1:8000/emp/read/101
+Method Type:GET
+Required Fields: None
+Access Type:Public
+*/
+
+router.get("/read/:eid",async(req,resp)=>{
+	//how to read url data
+	let emp_Id = req.params.eid;
+
+	//verify employee exist or not.
+
+	let employees = await getEmployee();
+	let employee = employees.find((employee)=>{return employee.eid == emp_Id})
+	if(!employee){
+	    return resp.json({"error":"Not Exits"})
+	}
+	return resp.json(employee)
+
+})
+
+
+/*
 URL : http://127.0.0.1:8000/emp/update/eid
 Method:PUT
 Required Fields:None 
 Access Type:Public 
 */
-router.put('/update/eid', (req, res) => {
+router.put('/update/:eid', async (req, res) => {
+    let emp_Id = req.params.eid;
+    let emp = req.body;
+    let employees = await getEmployee()
+    let employee = employees.find((employee) => {return employee.eid == emp_Id})
+    if(!employee) {
+        return res.json({"Error": "Employee Not Exits"})
+    }
+    let remaining_Employee = employees.filter(empl => empl.eid != emp_Id)
+    remaining_Employee.unshift(emp)
+    createEmployee(remaining_Employee)
    return res.json({"employee": "Employee Details updated success..!"})
 })
 
@@ -55,13 +89,13 @@ Access Type:Public
 */
 router.delete('/delete/:eid', async (req, res) => {
     let emp_id = req.params.eid;
-    console.log(emp_id);
+    //console.log(emp_id);
     let employees = await getEmployee();
-    let emp = employees.find(employee => employee.empId == emp_id)
+    let emp = employees.find(employee => employee.eid == emp_id)
     if(!emp){
         return res.json({"msg": "Employee Already exists"})
     }
-    let remining_Employee = employees.filter(employee => employee.empId != emp_id)
+    let remining_Employee = employees.filter(employee => employee.eid != emp_id)
     console.log(remining_Employee);
     await createEmployee(remining_Employee)
     return res.json({"Msg": "Employee Record Deleted!"})
