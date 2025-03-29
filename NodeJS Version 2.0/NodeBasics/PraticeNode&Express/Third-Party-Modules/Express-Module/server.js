e94e90a2a31417b289dc18c1c551388211c40bf0
Vasudev290 <vasukesavulu@gmail.com>
2025-03-29 15:36:19 +0530

    delete

--- NodeJS Version 2.0/NodeBasics/PraticeNode&Express/Third-Party-Modules/Express-Module/server.js
+++
@@ -1,8 +0,0 @@
-import Express from 'express';
-const app = Express()
-
-app.get('/', (req, res) => {
-    res.send("Hello Welcome to Node and Express js")
-}).listen(5000, () => {
-    console.log("Server Running Successfull...!")
-})
