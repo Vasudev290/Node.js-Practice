491626c104a87a21f0ccdc37e292f20c0cae0bb9
Vasudev290 <vasukesavulu@gmail.com>
2025-03-29 16:12:29 +0530

    router

--- NodeJS Version 2.0/BasicExpress/app.js
+++ NodeJS Version 2.0/BasicExpress/app.js
@@ -1,1 +1,12 @@
-console.log("Node & Express JS");
+const express = require('express');
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to Node and Express Js</h1>")
+})
+const port = 5000;
+
+app.listen(port, () => {
+    console.log("Server Started and Running Successfull..!")
+})
