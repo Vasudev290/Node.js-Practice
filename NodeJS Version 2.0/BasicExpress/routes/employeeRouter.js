90296a60a773555e9ccf02c4bacabd771548bad5
Vasudev290 <vasukesavulu@gmail.com>
2025-03-30 19:05:35 +0530

    CRUD

--- NodeJS Version 2.0/BasicExpress/routes/employeeRouter.js
+++ NodeJS Version 2.0/BasicExpress/routes/employeeRouter.js
@@ -4,7 +4,10 @@ const employeeController = require('../controller/employeeControler');
 const Employee = require('../models/Employee');
 
 //Api creations => ['get', "post", "put", 'delete']
-
+router.get('/all-emp', employeeController.readEmployee)
+router.get('/single-emp/:id', employeeController.singleEmployee)
 router.post('/add-emp', employeeController.createEmployee)
+router.put('/update-emp/:id', employeeController.updateEmployee)
+router.delete('/delete-emp/:id', employeeController.deleteEmployee )
 
 module.exports = router;
